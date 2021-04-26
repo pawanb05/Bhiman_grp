16ce929b368908f52b79b45a5ce516ef6f9ae93b
User <User@DESKTOP-MAM0QV8>
2021-04-26 10:44:06 +0530

    commiting Pawan to BranchX

---
+++ src/test/java/com/bhimangroup/regression/Pawan.java
@@ -0,0 +1,18 @@
+package com.bhimangroup.regression;
+
+public class Pawan 
+{
+	
+	public static void main(String[] args) {
+		
+		String s="hungry";
+	if(s=="hungry")
+	{
+		System.out.println("Eat First");
+	}
+	else
+	{
+		System.out.println("Working...");
+	}
+	}
+}
