3d6db71fc98ded85e72223ac0c93177309d225f7
User <User@DESKTOP-MAM0QV8>
2021-05-16 17:26:21 +0530

    Second Commit

---
+++ src/test/java/com/bhimangroup/regression/ReverseString.java
@@ -0,0 +1,33 @@
+package com.bhimangroup.regression;
+
+import java.util.Scanner;
+
+public class ReverseString 
+{
+	void reverse()
+	{
+		System.out.println("Enter String for reverse..");
+		Scanner sc = new Scanner(System.in);
+		String str=sc.nextLine();
+		String rev="";
+		int l=str.length();
+		
+		for (int i = l-1; i >=0 ;i--)
+		{
+			rev=rev+str.charAt(i);
+			
+		}
+		System.out.println("Reverse String is : "+rev);
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+
+}
