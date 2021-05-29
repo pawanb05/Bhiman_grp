6c3be1a0feec1cd13576ee4dba48105c14a7e5f2
User <User@DESKTOP-MAM0QV8>
2021-05-29 17:10:19 +0530

    Test Added

--- src/test/java/com/bhimangroup/regression/FBTestCaseLoginPage.java
+++ src/test/java/com/bhimangroup/regression/FBTestCaseLoginPage.java
@@ -1,10 +1,14 @@
 package com.bhimangroup.regression;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.bhimangroup.keywords.Constants;
 import com.bhimangroup.keywords.Keywords;
 import com.bhimangroup.pages.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class FBTestCaseLoginPage 
 {
@@ -20,10 +24,19 @@ public class FBTestCaseLoginPage
 		login.passwordTextBox("9876543210");
 		login.submitbtn();	
   }
-	@Test 
-	public void tearDown() 
+	@Test
+	public void logoDisplay() 
 	{
-	  Constants.driver.close();
+	 boolean element=Constants.driver.findElement(By.id("Pawan")).isDisplayed();
+	  Assert.assertTrue(true);
 	}
 
+	@Test
+	public void checkBoxSelected()
+	{
+		boolean s=Constants.driver.findElement(By.cssSelector("#check_box")).isSelected();
+		Assert.assertTrue(true, "Check_Box Is Selected");
+	}
+	
+	
 }
